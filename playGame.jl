b5b57ab166870a848e7495dc325c21035392ce3c
f18bc <42793728+f18bc@users.noreply.github.com>
2021-09-18 19:55:02 -0400

    Update playGame.jl

--- playGame.jl
+++ playGame.jl
@@ -1,10 +1,13 @@
-function playGame(deck,board,result)
+function playGame(deck,board)
   set = findAllSet(board)
   set,deck,board = selectOrAdd(set,deck,board)
-
-
-  if (isempty(set) && (length(deck)==0)) 
+  if (length(board)==0)
+    print("You Win!")
     return result
   end
-  playGame(deck,board,result)
+  if (length(deck)==0)
+    print("You Lose!")
+    return result
+  end
+  playGame(deck,board)
 end
