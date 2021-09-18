6eaf60e63f11a1ea1672a8f3c61ed2b0bdbad86f
f18bc <42793728+f18bc@users.noreply.github.com>
2021-09-18 19:58:48 -0400

    Update playGame.jl

--- playGame.jl
+++ playGame.jl
@@ -1,3 +1,13 @@
+include("cardBulder.jl")
+include("dealCard.jl")
+include("findAllSet.jl")
+include("formSetOrNot.jl")
+include("returnDeck.jl")
+include("selectOrAdd.jl")
+include("setGame.jl")
+include("shuffleDeck.jl")
+
+deck,board = setGame()
 function playGame(deck,board)
   set = findAllSet(board)
   set,deck,board = selectOrAdd(set,deck,board)
